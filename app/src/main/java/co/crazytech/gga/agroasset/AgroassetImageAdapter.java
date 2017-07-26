8dc5ee0d661d8b520f14e534dec724cfc5585f3a
Eric Wong <wymeric89@gmail.com>
2017-07-27 01:10:40 +0800

    new commit

--- app/src/main/java/co/crazytech/gga/agroasset/AgroassetImageAdapter.java
+++ app/src/main/java/co/crazytech/gga/agroasset/AgroassetImageAdapter.java
@@ -2,15 +2,26 @@ package co.crazytech.gga.agroasset;
 
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
 
 import co.crazytech.gga.R;
 
@@ -35,7 +46,20 @@ public class AgroassetImageAdapter extends PagerAdapter {
         View view = layoutInflater.inflate(R.layout.agroasset_image,container,false);
 
         ImageView imageView = (ImageView)view.findViewById(R.id.imageView);
-        Glide.with(context).load(imageFiles[position]).into(imageView);
+        File imageFile = imageFiles[position];
+        try {
+            if(!imageFile.exists()||imageFile.isDirectory()) throw new FileNotFoundException();
+            Glide.with(context).load(imageFile).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            Metadata imgMetadata = ImageMetadataReader.readMetadata(imageFile);
+            ExifSubIFDDirectory directory = imgMetadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+            Date dateTaken = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+            String dateStr = (String)DateFormat.format("dd/MM/yyyy EEE HH:mm:ss",dateTaken);
+            TextView textView = (TextView)view.findViewById(R.id.textView);
+            textView.setText(dateStr);
+        } catch (ImageProcessingException | IOException e) {
+//            Log.w("Image Exception",e.getMessage());
+        }
 
         container.addView(view);
         return view;
@@ -53,6 +77,6 @@ public class AgroassetImageAdapter extends PagerAdapter {
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        container.removeView((LinearLayout) object);
+        container.removeView((FrameLayout) object);
     }
 }
