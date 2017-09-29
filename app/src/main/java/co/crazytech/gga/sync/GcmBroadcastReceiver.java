911b99049190aa8360180cd6a21a6be12f47a8d5
wymeric <wymeric89@gmail.com>
2017-09-29 17:32:41 +0800

    added sync and GCM

---
+++ app/src/main/java/co/crazytech/gga/sync/GcmBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package co.crazytech.gga.sync;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by eric on 9/29/2017.
+ */
+
+public class GcmBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
